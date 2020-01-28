228ee2d9442fa76465d2772499c2391874733cc4
Jiu_book_ <863050404@qq.com>
2020-01-28 23:36:34 +0800

    Update MRSH.js
    更新字号大小判断

--- MRSH.js
+++ MRSH.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         McbbsReviewServerHelper
 // @namespace    https://space.bilibili.com/1501743
-// @version      0.0.1
+// @version      0.0.2
 // @description  MRSH - 你的服务器审核版好助手
 // @author       萌萌哒丶九灬书
 // @match        *://www.mcbbs.net/thread-*
@@ -44,6 +44,18 @@
             })
         }
     }
+
+    var flag_BodyTextSize = true;
+    //设置全局变量 BodyTextSize
+    function OnlyFalse(ele,str){
+        if(!ele){
+            flag_BodyTextSize = false;
+            str.html(function(i,origText){
+                return '❌' + origText;
+            })
+        }
+    }
+
     function ReviewTitleZZ(str){
         //正则判断标题
         var ZZ = /^\[(电信|联通|移动|双线|多线|教育|港澳|台湾|欧洲|美洲|亚太|内网)\]([\u4e00-\u9fa5]|\w|\s|[\u0800-\u4e00])*(\s|)——(\s|).[^\[]*\[(\d|\.|X|x|\-)+]$/;
@@ -89,28 +101,74 @@
         }
     }
 
+    function BodyFontSize(str){
+        var cssFontSize = str.css('font-size');
+        //找到font-size的css，并提取
+        var px = cssFontSize.indexOf('px');
+        //找到px字符的位置
+        var FontSize=cssFontSize.substring(0,px);
+        //将px切割，保留数字字符
+        return parseInt(FontSize);
+        //将string转换为int型，并返回
+    }
+
     jq(document).ready(function(){
         jq(function () {
-            TrueOrFalse(ReviewTitleZZ(jq('#thread_subject').text()),jq('#thread_subject'));
+            TrueOrFalse(ReviewTitleZZ(jq('#thread_subject').text()), jq('#thread_subject'));
             //通过正则表达式判断标题是否正确
             //console.log(jq('#thread_subject').text());
             //用于debug输出标题内容↑
 
-            TrueOrFalse(UserPointZZ(jq(".pil.cl dd").eq(2).text()),jq(".pil.cl dd").eq(2));
+            TrueOrFalse(UserPointZZ(jq(".pil.cl dd").eq(2).text()), jq(".pil.cl dd").eq(2));
             //eq(2)为贡献
             //console.log(jq(".pil.cl dd").eq(2).text());
             //用于debug输出贡献点↑
 
-            TrueOrFalse(UserPointZZ(jq(".pil.cl dd").eq(5).text()),jq(".pil.cl dd").eq(5))
+            TrueOrFalse(UserPointZZ(jq(".pil.cl dd").eq(5).text()), jq(".pil.cl dd").eq(5))
             //eq(5)为绿宝石
             //console.log(jq(".pil.cl dd").eq(5).text());
             //用于debug输出绿宝石↑
 
-            TrueOrFalse(ServerTitleName(jq('#thread_subject').text(),jq(".cgtl.mbm tbody tr td").eq(0).text()) >= 1 ,jq(".cgtl.mbm tbody tr td").eq(0))
+            TrueOrFalse(ServerTitleName(jq('#thread_subject').text(), jq(".cgtl.mbm tbody tr td").eq(0).text()) >= 1 ,jq(".cgtl.mbm tbody tr td").eq(0))
             //eq(0)为服务器名称
             //提取标题中的服务器名称后，和模板内服务器名称做对比
             //console.log(jq(".cgtl.mbm tbody tr td").eq(0).text());
             //用于debug输出服务器名称↑
+
+            jq('.t_f font font').each(function(){
+                OnlyFalse(BodyFontSize(jq(this)) <= 24, jq(this));
+            })
+            jq('.t_f font').each(function(){
+                OnlyFalse(BodyFontSize(jq(this)) <= 24, jq(this));
+            })
+            jq('.t_f div div div div').each(function(){
+                OnlyFalse(BodyFontSize(jq(this)) <= 24, jq(this));
+            })
+            jq('.t_f div div div').each(function(){
+                OnlyFalse(BodyFontSize(jq(this)) <= 24, jq(this));
+            })
+            jq('.t_f div div').each(function(){
+                OnlyFalse(BodyFontSize(jq(this)) <= 24, jq(this));
+            })
+            jq('.t_f div').each(function(){
+                OnlyFalse(BodyFontSize(jq(this)) <= 24, jq(this));
+            })
+            jq('.t_f a').each(function(){
+                OnlyFalse(BodyFontSize(jq(this)) <= 24, jq(this));
+            })
+            console.log(flag_BodyTextSize);
+            if(flag_BodyTextSize == false){
+                jq('.t_f').html(function(i,origText){
+                    return '<div align="center"><font color="red" size="4">❌当前页面中含有字符大小超过5的文字</font></div>' + origText;
+                });
+            }else{
+                jq('.t_f').html(function(i,origText){
+                    return '<div align="center"><font color="green" size="4">✅当前页面字符大小合规</font></div>' + origText;
+                });
+            }
+            //用于判断字符是否超过5号（24px）
+
+
         })
     });
 })();
